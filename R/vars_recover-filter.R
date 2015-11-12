8f4af67f379fa8c5b927d8e64fb7bb1d01726545
marc_comas <marc.comas@gmail.com>
2015-11-12 09:57:07 +0100

    Modifications to usrgi.lib package

--- R/vars_recover-filter.R
+++ R/vars_recover-filter.R
@@ -1,70 +1,80 @@
 ### FILTRES
 
-# Explicació:
-#   Filtra la variable tall, per a que tots els valors estiguin entre dos límits.
-# Input:
-#   - talla: variable on tenim la talla.
-#   - inf: límit inferior.
-#   - sup: límit superior.
-# Output:
-#   - talla: variable talla filtrada.
-filter.talla = function(talla, inf = 140, sup = 220){
-  valid = (inf <= talla & talla <= sup)
-  talla[!valid] = NA
-  talla
+#' Filter a variable eliminating measurements below and above a constant inf and sup respectively
+#' 
+#' @param value vector with measurements to be filtered
+#' @param inf lower bound
+#' @param sup upper bound
+#' @return the measurement vector filtered with NA in measurements outside the rank [ind, sup]
+#' 
+#' @export
+filter.variable = function(value, inf, sup){
+  valid = (inf <= value & value <= sup)
+  value[!valid] = NA
+  value
 }
 
-# Explicació:
-#   Filtra la variable pes, per a que tots els valors estiguin per sobre d'un límit inferior.
-# Input:
-#   - pes: variable on tenim la pes.
-#   - inf: límit inferior.
-# Output:
-#   - pes: variable pes filtrada.
-filter.pes = function(pes, inf = 40){
-  valid = (inf <= pes)
-  pes[!valid] = NA
-  pes
+
+#' Filter height variable eliminating measurements below and above a constant inf and sup respectively
+#' 
+#' @param value vector with height measurements to be filtered
+#' @param inf lower bound (default 140)
+#' @param sup upper bound (default 220)
+#' @return the measurement vector filtered with NA in measurements outside the rank [ind, sup]
+#' 
+#' @export
+filter.height = function(height, inf = 140, sup = 220){
+  filter.variable(height, inf, sup)
 }
 
-# Explicació:
-#   Filtra la variable IMC, per a que tots els valors estiguin entre dos límits, exceptuant les persones que 
-#   siguin obeses que no tenen límit superior.
-# Input:
-#   - imc: variable on tenim la IMC.
-#   - obesity: variable indicadora on tenim recollida la informació sobre la obesitat.
-#              Per defecte, NULL.
-#   - inf: límit inferior.
-#   - sup: límit superior.
-# Output:
-#   - imc: variable imc filtrada.
-filter.imc = function(imc, obesity = NULL, inf = 14, sup = 40){
-  valid = (inf <= imc & imc <= sup) | ifelse(is.null(obesity), FALSE, (imc > sup & obesity == 1))
-  imc[!valid] = NA
-  imc
+#' Filter weight variable eliminating measurements below and above a constant inf and sup respectively
+#' 
+#' @param value vector with weight measurements to be filtered
+#' @param inf lower bound (default 140)
+#' @param sup upper bound (default 220)
+#' @return the measurement vector filtered with NA in measurements outside the rank [ind, sup]
+#' 
+#' @export
+filter.weight = function(weight, inf = 40, sup=Inf){
+  filter.variable(weight, inf, sup)
 }
 
-# Explicació:
-#   Filtra les dues variables sobre la tensió arterial (tas i tad), 
-#   per a que tots els valors estiguin entre dos límits.
-# Input:
-#   - tas: variable on tenim la tas.
-#   - tad: variable on tenim la tas.
-#   - inf.tas: límit inferior de tas.
-#   - sup.tas: límit superior de tas.
-#   - inf.tad: límit inferior de tad.
-#   - sup.tad: límit superior de tad.
-# Output:
-#   - tas: variable tas filtrada.
-#   - tad: variable tad filtrada.
-filter.bp = function(tas, tad, inf.tas = 60, inf.tad = 30, sup.tas = 250, sup.tad = 170){
-  valid = sup.tas >= tas & tas >= inf.tas
-  tas[!valid] = NA
-  valid = sup.tad >= tad & tad >= inf.tad
-  tad[!valid] = NA
-  no.valid = !is.na(tas) & !is.na(tad) & tas <= tad
-  tad[no.valid] = NA
-  cbind(tas, tad)
+#' Filter bmi variable eliminating measurements below and above a constant inf and sup respectively
+#' 
+#' @param value vector with bmi measurements to be filtered
+#' @param obesity indicative variable of obesity
+#' @param inf lower bound (default 140)
+#' @param sup upper bound (default 220)
+#' @return the measurement vector filtered with NA in measurements outside the rank [ind, sup]
+#' 
+#' @export
+filter.bmi = function(bmi, obesity = NULL, inf = 14, sup = 40){
+  valid = (inf <= bmi & bmi <= sup) | ifelse(is.null(obesity), FALSE, (bmi > sup & obesity == 1))
+  bmi[!valid] = NA
+  bmi
+}
+
+
+#' Filter bp variable eliminating measurements below and above a constant inf and sup respectively and with coherent measurement
+#' between sbp and dbp
+#' 
+#' @param sbp vector with bmi measurements to be filtered
+#' @param dbp indicative variable of obesity
+#' @param sbp.inf lower bound (default 60)
+#' @param dbp.inf lower bound (default 30)
+#' @param sbp.sup upper bound (default 250)
+#' @param dbp.sup upper bound (default 170)
+#' @return the measurement vector filtered with NA in measurements outside the rank [ind, sup]
+#' 
+#' @export
+filter.bp = function(sbp, dbp, inf.sbp = 60, inf.dbp = 30, sup.sbp = 250, sup.dbp = 170){
+  valid = sup.sbp >= sbp & sbp >= inf.sbp
+  sbp[!valid] = NA
+  valid = sup.dbp >= dbp & dbp >= inf.dbp
+  dbp[!valid] = NA
+  no.valid = !is.na(sbp) & !is.na(dbp) & sbp <= dbp
+  dbp[no.valid] = NA
+  cbind(sbp, dbp)
 }
 
 # Explicació:
@@ -88,7 +98,25 @@ filter.bp = function(tas, tad, inf.tas = 60, inf.tad = 30, sup.tas = 250, sup.ta
 #   - colhdl: variable colhdl filtrada.
 #   - colldl: variable colldl filtrada.
 #   - tg: variable tg filtrada.
-filter.colesterol = function(coltot, colhdl, colldl, tg, 
+
+#' Filter liidic variables (coltot, colhdl, colldl, tg)
+#' 
+#' @param coltot coltot variable
+#' @param colhdl colhdl variable
+#' @param colldl colldl variable
+#' @param tg tg variable
+#' @param inf.tot coltot inferior limit
+#' @param sup.tot coltot superior limit
+#' @param inf.hdl colhdl inferior limit
+#' @param sup.hdl colhdl superior limit
+#' @param inf.ldl colldl inferior limit
+#' @param sup.ldl colldl superior limit
+#' @param inf.tg tg inferior limit
+#' @param sup.tg tg superior limit
+#' @return coltot,colhdl,colldl and tg filtered
+#' 
+#' @export
+filter.cholesterol = function(coltot, colhdl, colldl, tg, 
                              inf.tot = 90, sup.tot = 450, inf.hdl = 25, sup.hdl = 150, 
                              inf.ldl = 50, sup.ldl = 295, inf.tg = 25, sup.tg = 2000){
   non.valid = !is.na(coltot) & ( coltot < inf.tot | coltot > sup.tot)
@@ -104,42 +132,52 @@ filter.colesterol = function(coltot, colhdl, colldl, tg,
 
 ### RECOVERS
 
-# Explicació:
-#   Recuperem IMC a partir de la informació de la talla i el pes.
-# Input:
-#   - imc: variable IMC original.
-#   - talla: variable talla.
-#   - pes: variable pes.
-# Output:
-#   - imc: IMC recuperat.
-recover.imc = function(imc, talla, pes){
-  select = is.na(imc) & !is.na(talla) & !is.na(pes) & talla != 0
+#' Recover bmi variable from height and weight
+#' 
+#' @param bmi vector with bmi measurements to be filtered
+#' @param height height in cm
+#' @param weight weight in kg
+#' @return bmi with recovered measurements
+#' 
+#' @export
+recover.bmi = function(bmi, height, weight){
+  select = is.na(bmi) & !is.na(height) & !is.na(weight) & height != 0
   if(length(select) != 0)
-    imc[select] = pes[select] / (talla[select]/100)^2
-  imc
+    bmi[select] = weight[select] / (height[select]/100)^2
+  bmi
 }
+
+#' Recover obesity from BMI calculation
+#' 
+#' @param obesity original indicator variable for obesity condition
+#' @param bmi vector with bmi measurements
+#' @return recovered indicator variable for obesity condition
+#' 
+#' @export
 recover.obesity = function(obesity, imc){
   as.numeric(obesity == 1 | (!is.na(imc) & imc > 30))
 }
 
-# Explicació:
-#   Recuperem la informació de les variables de tabac a partir d'informació sobre tabac.
-# Input:
-#   - smoking: tabac per icd (problemes ecap o cmbd).
-#   - tabac : variables ecap.
-#   - t_tabac : temp fins a mesura variable.
-#   - non_smoker_time : temps que fa que no fuma.
-# Output:
-#   - new_smoking: variable smoking resum.
-recover.smoking = function(smoking, tabac, t_tabac, non_smoker_time = 365, na.value = 0){
+
+#' Recover smoking
+#' 
+#' @param smoking original indicator variable for smoking condition
+#' @param smoke smoking condition (0: never, 1: smoker, 2: previous smoker)
+#' @param t_smoke days since  smoking condition
+#' @param non_smoker_time days since a person is considered non-smoker (default: 365)
+#' @param na.value value to be assigned to people with missing in smoke variable
+#' @return recovered indicator variable for smoking condition
+#' 
+#' @export
+recover.smoking = function(smoking, smoke, t_smoke, non_smoker_time = 365, na.value = 0){
   new_smoking = smoking
-  sel1 = ( !is.na(tabac) & (tabac == 1) )
-  sel2_smoker = ( !is.na(tabac) & (tabac == 2) & (t_tabac < non_smoker_time) )
-  sel2_non_smoker = ( !is.na(tabac) & (tabac == 2) & (t_tabac >= non_smoker_time) )
+  sel1 = ( !is.na(smoke) & (smoke == 1) )
+  sel2_smoker = ( !is.na(smoke) & (smoke == 2) & (t_smoke < non_smoker_time) )
+  sel2_non_smoker = ( !is.na(smoke) & (smoke == 2) & (t_smoke >= non_smoker_time) )
   new_smoking[sel1] = 1
   new_smoking[sel2_smoker] = 1
   new_smoking[sel2_non_smoker] = 0
-  new_smoking[new_smoking == 0 & is.na(tabac)] = na.value
+  new_smoking[new_smoking == 0 & is.na(smoke)] = na.value
   new_smoking
 }
 
@@ -154,6 +192,17 @@ recover.smoking = function(smoking, tabac, t_tabac, non_smoker_time = 365, na.va
 #   - filter: després de recuperar tornem a fer el filtre. Per defecte, TRUE.
 # Output:
 #   - matriu amb els colesterols.
+
+#' Recover cholesterols
+#' 
+#' @param coltot
+#' @param colhdl
+#' @param colldl 
+#' @param tg 
+#' @param filter apply the filter after recovering (default TRUE)
+#' @return recovered cholesterols
+#' 
+#' @export
 recover.colesterol = function(coltot, colhdl, colldl, tg, filter = TRUE){  
   tot = as.numeric(is.na(coltot))
   hdl = as.numeric(is.na(colhdl))
@@ -180,15 +229,16 @@ recover.colesterol = function(coltot, colhdl, colldl, tg, filter = TRUE){
   }
 }
 
-# Explicació:
-#   Recuperem la informació de les variables de alcohol
-# Input:
-#   - alcoholism (problema ecap o cmbd).
-#   - alcohol 0: no beu, 1: beu, 2: bevedor de risc.
-#   - deafult: valor que posem per defecte, nivells de la variable alcohol. Per defecte, 2.
-# Output:
-#   - alcoholism_new: variable alcohol resum.
-recover.alcoholism = function(alcoholism, alcohol, default = 2, na.value = 0){
+
+#' Recover alcoholism
+#' 
+#' @param alcoholism original indicator variable for smoking condition
+#' @param alcohol alcohol condition (0: no drinker, 1: low risk drinker, 2: high risk drinker)
+#' @param na.value value to be assigned to people with missing in alcoholism variable
+#' @return recovered indicator variable for alcoholism condition from high risk drinker
+#' 
+#' @export
+recover.alcoholism = function(alcoholism, alcohol, na.value = 0){
   alcoholism_new = alcoholism
   alcoholism_new[!is.na(alcohol) & alcohol == 2] = 1
   alcoholism_new[alcoholism_new == 0 & is.na(alcohol)] = na.value
@@ -202,24 +252,14 @@ recover.alcoholism = function(alcoholism, alcohol, default = 2, na.value = 0){
 #   - meds: medicaments relacionats amb el problema.
 # Output:
 #   - variable recuperada.
+
+#' Recover alcoholism
+#' 
+#' @param problem indicator variable for a given problem
+#' @param meds matrix with indicator facturation associated to the problem
+#' @return recovered indicator variable for a given problem
+#' 
+#' @export
 recover.using_medication = function(problem, meds){
   as.numeric(problem == 1 | apply( as.data.frame(meds), 1, sum) > 0 )
-}
-
-# Explicació:
-#   Recuperem informació agregant segons la variable agg i aplicant la funció funct
-# Input:
-#   - x: variable que volem recuperar.
-#   - agg: grup que s'utilitzarà per recuperar x
-#   - funct: funció que s'aplica a cada grup. El resultat s'utilitzarà per recuperar x
-# Output:
-#   - variable recuperada.
-impute_by_aggregation = function(x, agg, funct, ...){
-  X = aggregate(as.matrix(x)~agg, FUN=funct, ...)
-  x.agg = X[,2]
-  names(x.agg) = X[,1]  
-  x.na = is.na(x)
-  x[x.na] = x.agg[agg[x.na]]
-  x[is.na(x)] = mean(x, na.rm=TRUE)
-  return(x)
 }
